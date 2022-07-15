09d51955faa29e9e92bdd63c6eae01534ac85908
horbachova <gor.jenia2003@gmal.com>
2022-07-15 03:08:00 -0600

    added form deactivation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { offersArray } from './data.js';
 import { renderCard } from './popup.js';
+import {makeDisabled} from './form.js';
 
 const mapCanvas = document.querySelector('#map-canvas');
 mapCanvas.append(renderCard(offersArray[0]));
+
