8ccafcccbc80e32b88e360e0a28e01950f9e9a0d
horbachova <gor.jenia2003@gmal.com>
2022-07-16 23:25:31 -0600

    added form validation

--- js/form.js
+++ js/form.js
@@ -1,36 +1,61 @@
-
-// Неактивное состояние. При открытии страница находится в неактивном состоянии:
-
-// На месте карты отображается серый прямоугольник.
-
-// Форма заполнения информации об объявлении .ad-form содержит класс ad-form--disabled;
-
-// Все интерактивные элементы формы .ad-form должны быть заблокированы с помощью атрибута disabled,
-// добавленного на них или на их родительские блоки fieldset. Слайдер также должен быть заблокирован;
-// Форма с фильтрами .map__filters заблокирована так же, как и форма .ad-form — на форму добавлен специальный класс,
-// а на её интерактивные элементы атрибуты disabled.
-
 const adForm = document.querySelector('.ad-form');
-const mapFilters = document.querySelector('.map__filters');
-const disabledFields = document.querySelectorAll('select.map__filter, fieldset');
-const address = adForm.querySelector('#address');
+// const title = adForm.querySelector('#title');
+const type = adForm.querySelector('#type');
+const price = adForm.querySelector('#price');
+const time = adForm.querySelector('#timein');
+const timeIn = adForm.querySelector('#timein');
+const timeOut = adForm.querySelector('#timeout');
+const capacity = adForm.querySelector('#capacity');
+const roomNumber = adForm.querySelector('#room_number');
+const guestCapacity = capacity.querySelectorAll('option');
 
-const disabledToggle = () => {
-  disabledFields.forEach((line) => {
-    line.disabled = !line.disabled;
+const typeOfHouse = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+
+};
+
+const priceChangeHandler = () => {
+  const minPrice = typeOfHouse[type.value];
+  price.min = minPrice;
+  price.placeholder = minPrice;
+};
+
+type.addEventListener('change', priceChangeHandler);
+
+const timeSetHandler = () => {timeOut.value = timeIn.value;
+};
+
+time.addEventListener('change', timeSetHandler);
+
+
+const numberOfGuests = {
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0'],
+};
+
+
+const validateRooms = () => {
+  const roomValue = roomNumber.value;
+
+  guestCapacity.forEach((guest) => {
+    const isDisabled = !(numberOfGuests[roomValue].includes(guest.value));
+    guest.selected =  numberOfGuests[roomValue][0] === guest.value;
+    guest.disabled = isDisabled;
+    guest.hidden = isDisabled;
   });
 };
 
-const makeDisabled = () => {
-  adForm.classList.toggle('ad-form--disabled');
-  mapFilters.classList.toggle('map__filters--disabled');
+validateRooms();
 
-  disabledToggle();
-  address.setAttribute('readonly', true);
-  address.value = "35.68950, 139.69200"
-
+const roomNumberChangeHandler = () => {
+  validateRooms();
 };
 
-export {makeDisabled};
-
+roomNumber.addEventListener('change', roomNumberChangeHandler);
 
--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 import { offersArray } from './data.js';
 import { renderCard } from './popup.js';
-import {makeDisabled} from './form.js';
+// import {makeDisabled} from './map.js';
+import './form.js'
 
 const mapCanvas = document.querySelector('#map-canvas');
 mapCanvas.append(renderCard(offersArray[0]));
+
+
+
 
