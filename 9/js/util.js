46c73c958a0cad0c0c11ad7c85752a901f8cdb01
Keks <keks@htmlacademy.ru>
2022-07-26 11:01:29 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/util.js
@@ -0,0 +1,61 @@
+//Calculate random numbers
+const getRandomInt = (min, max) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+  return (Math.round(Math.random() * (max - min) + min));
+};
+
+getRandomInt();
+
+const getRandomFloat = (min, max, digits = 1) => {
+  if (min < 0 || max < 0) {
+
+    return -1;
+  }
+  return ((Math.random() * (max - min) + min).toFixed(digits));
+};
+
+getRandomFloat();
+
+const getImageAddress = (number) => {
+  if (number >= 10) {
+    return `img/avatars/user${number}.png`;
+  }
+  return `img/avatars/user0${number}.png`;
+};
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+const numDecline = (num, nominative, genitiveSingular, genitivePlural) => {
+  if(num > 10 && (Math.round((num % 100) / 10)) === 1){
+    return genitivePlural;
+  }
+  switch(num % 10){
+    case 1: return nominative;
+    case 2:
+    case 3:
+    case 4: return genitiveSingular;
+  }
+  return genitivePlural;
+};
+
+const getAddressCoordinates = (form, coordinates) => {
+  form.value = `${(coordinates.lat).toFixed(5)}, ${(coordinates.lng).toFixed(5)}`;
+};
+
+const debounce = (cb, timeout = 500) => {
+  let timer;
+  return (...rest) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { cb.apply(this, rest); }, timeout);
+  };
+};
+
+export { numDecline, getRandomInt, getRandomFloat, getImageAddress, shuffleArray, getAddressCoordinates, debounce };
